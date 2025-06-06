44c780cc35f0ac1470c59d106e2ecc46498b0f05
sakharedev <parassakhare72727@gmail.com>
2025-06-07 01:26:19 +0530

    added roarm things

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -220,7 +220,96 @@ class So100DataConfig(BaseDataConfig):
 
 ###########################################################################################
 
+class RoArmDataConfig(BaseDataConfig):
+    video_keys = ["video.phone", "video.laptop"]
+    state_keys = [
+        "state.motor_0",
+        "state.motor_1",
+        "state.motor_2",
+        "state.motor_3",
+        "state.motor_4",
+        "state.motor_5",
+    ]
+    action_keys = [
+        "action.motor_0",
+        "action.motor_1",
+        "action.motor_2",
+        "action.motor_3",
+        "action.motor_4",
+        "action.motor_5",
+    ]
+    language_keys = ["annotation.human.task_description"]
+    observation_indices = [0]
+    action_indices = list(range(16))
+    def modality_config(self):
+        video_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.video_keys,
+        )
+        state_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.state_keys,
+        )
+        action_modality = ModalityConfig(
+            delta_indices=self.action_indices,
+            modality_keys=self.action_keys,
+        )
+        language_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.language_keys,
+        )
+        modality_configs = {
+            "video": video_modality,
+            "state": state_modality,
+            "action": action_modality,
+            "language": language_modality,
+        }
+        return modality_configs
+    def transform(self):
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes={key: "min_max" for key in self.state_keys},
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes={key: "min_max" for key in self.action_keys},
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                action_concat_order=self.action_keys,
+            ),
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=32,
+            ),
+        ]
 
+        return ComposedModalityTransform(transforms=transforms)
+    
+
+
+###########################################################################################
 class Gr1FullUpperBodyDataConfig(BaseDataConfig):
     video_keys = ["video.front_view"]
     state_keys = [
@@ -682,4 +771,5 @@ DATA_CONFIG_MAP = {
     "bimanual_panda_hand": BimanualPandaHandDataConfig(),
     "single_panda_gripper": SinglePandaGripperDataConfig(),
     "so100": So100DataConfig(),
+    "roarm_m3": RoArmDataConfig()
 }
